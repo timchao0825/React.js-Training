3d951d0119c773d0ab6bd62d283a84cbd1e0ba3e
Tim <tim.chao0825@gmail.com>
2020-04-05 16:22:01 +0800

    filter lessons fetch API pratice

--- src/filter-lesson/App.js
+++ src/filter-lesson/App.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 
-import lessons from './lessons.json';
+// 純前端作法
+// import lessons from './lessons.json';
 
 class App extends Component {
   state = {
     filter:'',
-    lessons,
+    // 純前端作法
+    // lessons,
+    lessons:[]
+  }
+  componentDidMount(){
+    this.fetechLessons();
   }
   onChangeFilter = (e) => {
-    this.setState({
-      filter:e.target.value,
-    })
+    const filter = e.target.value;
+    this.setState({ filter } , this.fetechLessons());
   }
+
+  fetechLessons = () => {
+    let timer;
+    clearTimeout(timer);
+    timer = setTimeout(()=>{
+      // 後端做法要開啟express server
+      // yarn server 
+      const {filter} = this.state;
+      fetch(`/api/lessons/?filter=${filter}`)
+      .then(rs=>rs.json())
+      .then(lessons=>this.setState({lessons}));
+    },400);
+  }
+
   render() {
     const {filter,lessons} = this.state;
     // 純前端作法
-    const visibleLessons = lessons.filter((lesson)=>{
-      return lesson.title
-              .toLowerCase()
-              .includes(filter.toLowerCase());
-    });
+    // const visibleLessons = lessons.filter((lesson)=>{
+    //   return lesson.title
+    //           .toLowerCase()
+    //           .includes(filter.toLowerCase());
+    // });
     return (
       <div>
         <input type="text" value={filter} onChange={this.onChangeFilter}/>
         <ul>
-          {visibleLessons.map(
+          {/* 純前端作法 */}
+          {/* lessons 要改成 visibleLessons */}
+          {lessons.map(
             (lesson)=>{
               return <li key={lesson.id}>{lesson.title}</li>
             }
